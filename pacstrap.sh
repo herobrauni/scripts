8e46c3b11e2548ba04b551adb34fb8c351487dcd
herobrauni <braunnicolaj@gmail.com>
2019-10-15 13:25:58 +0200

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -6,7 +6,7 @@ set -e
 pacstrap /mnt base base-devel linux btrfs-progs snapper \
 zsh mlocate htop net-tools wireless_tools wpa_supplicant \
 dialog nano sudo intel-ucode grub bash-completion git \
-ansible
+ansible efibootmgr
 
 genfstab -L -p /mnt >> /mnt/etc/fstab
 
