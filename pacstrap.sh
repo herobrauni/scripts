2a5ba46fa1fa822838b8f80b477580dd9a795a47
herobrauni <braunnicolaj@gmail.com>
2019-10-15 13:40:04 +0200

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -11,11 +11,10 @@ ansible efibootmgr
 genfstab -L -p /mnt >> /mnt/etc/fstab
 
 echo "KEYMAP=de" > /mnt/etc/vconsole.conf
-echo "en_US.UTF-8" >> /mnt//etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo FONT=Lat2-Terminus16 >> /mnt/etc/vconsole.conf
 
 arch-chroot /mnt /bin/bash locale-gen
-echo "en_US.UTF-8 UTF-8" >> /mnt//etc/locale.gen
 echo LANGUAGE=en_US >> /mnt/etc/locale.conf
 echo LC_ALL=C >> /mnt/etc/locale.conf
 
