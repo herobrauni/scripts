cf17d2e35820cce46985b55d29a8f406f3ab8f69
herobrauni <braunnicolaj@gmail.com>
2019-10-15 11:03:06 +0200

    Create pacstrap.sh

---
+++ pacstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# install base programs
+
+set -e
+
+pacstrap /mnt base base-devel btrfs-progs snapper \
+zsh mlocate htop net-tools wireless_tools wpa_supplicant \
+dialog nano sudo intel-ucode grub bash-completion git \
+ansible
+
+genfstab -L -p /mnt >> /mnt/etc/fstab
