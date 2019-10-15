dce544d42981a05f07adee6a0d323d0c331703f3
herobrauni <braunnicolaj@gmail.com>
2019-10-15 09:53:03 +0200

    Update partitioning.sh

--- partitioning.sh
+++ partitioning.sh
@@ -10,20 +10,20 @@ sleep 30
 echo "Formating"
 
 sgdisk --zap-all "$DRIVE"
-
+&&
 echo -e "n\n\n\n512M\nef00\nn\n\n\n\n\nw\ny" | gdisk "$DRIVE" &> /dev/null
-
+&&
 BOOT="${DRIVE}1"
 ROOT="${DRIVE}2"
 
 echo "Formating Boot as fat32"
 wipefs -a "$BOOT"
 mkfs.vfat -F32 "$BOOT"
-
+&&
 echo "Formating Root as btrfs"
-mkfs.btrfs -L archlinux "$ROOT"
 wipefs -a "$ROOT"
-
+mkfs.btrfs -L archlinux "$ROOT"
+&&
 echo "Create Subvols"
 mount -t btrfs -o compress=lzo "$ROOT" /mnt
 btrfs subvolume create /mnt/@
@@ -31,19 +31,19 @@ btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@snapshots
 btrfs subvolume create /mnt/@homeshots
 btrfs subvolume create /mnt/@swap
-
+&&
 umount /mnt
-
+&&
 echo "Mounting Subvols"
 mount -o subvol=@,ssd,compress=lzo,discard "$ROOT" /mnt
 mkdir /mnt/{home swap .snapshots boot}
 mkdir /mnt/home/.snapshots
-
+&&
 mount -o subvol=@home,ssd,compress=lzo,discard "$ROOT" /mnt/home
 mount -o subvol=@swap,ssd,compress=lzo,discard "$ROOT" /mnt/swap
 mount -o subvol=@snapshots,ssd,compress=lzo,discard "$ROOT" /mnt/.snapshots
 mount -o subvol=@homeshots,ssd,compress=lzo,discard "$ROOT" /mnt/home/.snapshots
-
+&&
 echo "Create SWAP"
 truncate -s 0 /mnt/swap/swapfile
 chattr +C /mnt/swap/swapfile
@@ -51,8 +51,8 @@ dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192 status=progress
 chmod 600 /mnt/swap/swapfile
 mkswap /mnt/swap/swapfile
 swapon /mnt/swap/swapfile
-
+&&
 echo "Mount boot"
 mount "$BOOT" /mnt/boot
-
+&&
 genfstab -L -p /mnt >> /mnt/etc/fstab
