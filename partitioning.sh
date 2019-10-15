cfb092897f1d610ed99197abe520e0097f849e0c
herobrauni <braunnicolaj@gmail.com>
2019-10-15 09:29:27 +0200

    Create partitioning.sh

---
+++ partitioning.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Formats DRIVE into boot /dev/sdX1, LUKS encrypts /dev/sdX2
+# sets up btrfs with subvolumes on /dev/sdX2
+
+DRIVE=$1
+
+echo "HDD $DRIVE"
+sleep 30
+echo "Formating"
+
+echo -e "n\n\n\n512M\nef00\nn\n\n\n\n\nw\ny" | gdisk /dev/"$DRIVE" &> /dev/null
+
+BOOT="${DRIVE}1"
+ROOT="${DRIVE}2"
+
+echo "Formating Boot as fat32"
+sgdisk --zap-all /dev/"$BOOT"
+wipefs -a /dev/"$BOOT"
+mkfs.vfat -F32 /dev/"$BOOT")
+
+echo "Formating Root as btrfs"
+mkfs.btrfs -L archlinux /dev/"$ROOT"
+sgdisk --zap-all /dev/"$ROOT"
+wipefs -a /dev/"$ROOT")
+
+echo "Create Subvols"
+mount -t btrfs -o compress=lzo "$ROOT" /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+btrfs subvolume create /mnt/@homeshots
+btrfs subvolume create /mnt/@swap
+
+umount /mnt
+
+echo "Mounting Subvols"
+mount -o subvol=@,ssd,compress=lzo,discard "$ROOT" /mnt
+mkdir /mnt/{home swap .snapshots boot}
+mkdir /mnt/home/.snapshots
+
+mount -o subvol=@home,ssd,compress=lzo,discard "$ROOT" /mnt/home
+mount -o subvol=@swap,ssd,compress=lzo,discard "$ROOT" /mnt/swap
+mount -o subvol=@snapshots,ssd,compress=lzo,discard "$ROOT" /mnt/.snapshots
+mount -o subvol=@homeshots,ssd,compress=lzo,discard "$ROOT" /mnt/home/.snapshots
+
+echo "Create SWAP"
+truncate -s 0 /mnt/swap/swapfile
+chattr +C /mnt/swap/swapfile
+dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192 status=progress
+chmod 600 /mnt/swap/swapfile
+mkswap /mnt/swap/swapfile
+swapon /mnt/swap/swapfile
+
+echo "Mount boot"
+mount /dev/sda1 /mnt/boot
+
+genfstab -L -p /mnt >> /mnt/etc/fstab
