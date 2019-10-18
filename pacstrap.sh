2c18ac7e8379697fa15a1d3fd04e30d5bf13b60d
herobrauni <braunnicolaj@gmail.com>
2019-10-18 16:13:11 +0200

    removed xcp for now

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-
 set -e
 
 pacstrap /mnt base base-devel linux btrfs-progs snapper \
 zsh mlocate htop net-tools wireless_tools wpa_supplicant \
 dialog nano sudo intel-ucode grub bash-completion git \
 ansible efibootmgr yay neofetch zsh-completions \
-ripgrep fd xcp bat diskus pdfpc sl
+ripgrep fd bat diskus pdfpc sl
+
+# xcp
