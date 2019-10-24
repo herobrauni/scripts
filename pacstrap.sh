e83debca555494e898a2b8b22be6d6a5fbe4b386
herobrauni <braunnicolaj@gmail.com>
2019-10-24 09:50:19 +0200

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -8,4 +8,13 @@ dialog nano sudo intel-ucode grub bash-completion git \
 ansible efibootmgr yay neofetch zsh-completions \
 ripgrep fd bat diskus pdfpc sl
 
+#xfce + customizations
+
+pacstrap /mnt xfce4 xfce4-goodies \
+xdg-user-dirs xorg-server xorg-apps xorg-xinit xterm \
+ttf-dejavu gvfs gvfs-smb gvfs-mtp \
+pulseaudio pavucontrol pulseaudio-alsa alsa-utils unzip \
+gvfs arc-gtk-theme elementary-icon-theme numix-icon-theme-git \
+numix-circle-icon-theme-git htop lynx \
+libreoffice-fresh vlc gnome-packagekit pantheon-music
 # xcp
