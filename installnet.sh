440a58036a4911902786ffa4d7292dda16f51fb8
herobrauni <braunnicolaj@gmail.com>
2024-05-26 21:09:17 +0200

    Update installnet.sh

--- installnet.sh
+++ installnet.sh
@@ -710,6 +710,7 @@ in-target /bin/sh -c "echo '${hostnamevar}' > /etc/hostname"; \
 in-target chown -R debian /home/debian/.ssh/; \
 in-target chmod 644 /home/debian/.ssh/authorized_keys; \
 in-target chmod 700 /home/debian/.ssh/; \
+in-target systemctl enable systemd-resolved; \
 cp /saved/resolv.conf /target/etc/resolv.conf; \
 cp /saved/hosts /target/etc/hosts; \
 echo '@reboot root cat /etc/run.sh 2>/dev/null |base64 -d >/tmp/run.sh; rm -rf /etc/run.sh; sed -i /^@reboot/d /etc/crontab; bash /tmp/run.sh' >>/target/etc/crontab; \
