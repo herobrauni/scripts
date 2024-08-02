019776bd38552a24092f770ef9af54259a6fabb1
brauni <braunnicolaj@gmail.com>
2024-08-02 22:47:35 +0200

    installer

--- installer/ansible.sh
+++ installer/ansible.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-paru -S --needed --noconfirm ansible
+#paru -S --needed --noconfirm ansible
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
