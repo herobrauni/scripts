607ee7df281eaa67fd1728a2ab3a74feedc8d4b7
brauni@bara <braunnicolaj@gmail.com>
2024-08-03 16:41:17 +0200

    update

--- nix-home/0-init.sh
+++ nix-home/0-init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-#paru -S --needed --noconfirm ansible
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+git clone git@github.com:herobrauni/nixos.git ~/.config/home-manager
+
+#paru -S --needed --noconfirm ansible
