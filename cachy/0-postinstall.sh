ba8275a81167e073dbde86afa98b161c06e69e0f
brauni@bara <braunnicolaj@gmail.com>
2024-08-04 14:40:28 +0200

    bla

--- cachy/0-postinstall.sh
+++ cachy/0-postinstall.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-paru -S --needed --noconfirm libfido2 ansible
-
-eval "$(ssh-agent -s)"
-ssh-add -K
-mkdir ~/.ssh
-cd ~/.ssh
-ssh-keygen -K
-
-mv id_ed25519_sk_rk id_ed25519_sk
-mv id_ed25519_sk_rk.pub id_ed25519_sk.pub
-
+paru -S --needed --noconfirm libfido2 ansible proxzima-plymouth-git
+sudo plymouth-set-default-theme -R proxzima
 
